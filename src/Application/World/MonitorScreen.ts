29d058292fe855a9f2a09882e18fa026c4e80e30
jodzOMBIE <93054307+jodzOMBIE@users.noreply.github.com>
2025-08-21 13:52:02 +0200

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://simonglaeve.de/';
+        iframe.src = 'https://hit-me.media/gridlives/';
         /**
          * Use dev server is query params are present
          *
