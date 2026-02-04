32e6903442158d7367319b8f20ee90a0d325dc13
jodzOMBIE <93054307+jodzOMBIE@users.noreply.github.com>
2026-02-04 19:51:31 +0100

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://hit-me.media/gridlives/';
+        iframe.src = 'https://hipi-c.com';
         /**
          * Use dev server is query params are present
          *
